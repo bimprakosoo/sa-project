400ec0aefdc13b06500cc8ba55a7f492a0b1d620
ALOLA <omibalola@gmail.com>
2018-07-23 19:03:55 +0700

    scan blackcell function + init map

--- AkariPuzzle/src/akaripuzzle/AkariPuzzle.java
+++ AkariPuzzle/src/akaripuzzle/AkariPuzzle.java
@@ -6,65 +6,75 @@
 package akaripuzzle;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 
 /**
  *
- * @author ALOLA
+ * @author Alola
  */
 public class AkariPuzzle {
 
-    public static char[][] board = {
+//    public static char[][] board = {
+//        {'E', 'E', '1', 'E', 'E', 'B', 'E'},
+//        {'3', 'E', 'E', 'E', 'E', 'E', 'E'},
+//        {'E', 'E', 'E', 'E', 'E', 'E', 'B'},
 //        {'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//        {'B', 'E', 'E', 'E', 'E', 'E', 'E'},
+//        {'E', 'E', 'E', 'E', 'E', 'E', '3'},
+//        {'E', '2', 'E', 'E', 'B', 'E', 'E'},};
+//    public static char[][] board = {
 //        {'E', 'E', 'E', 'E', 'E', 'B', 'E'},
-//        {'E', 'E', 'E', '3', 'E', 'E', 'E'},
-//        {'E', 'E', 'B', 'B', '2', 'E', 'E'},
-//        {'E', 'E', 'E', '3', 'E', 'E', 'E'},
-//        {'E', '2', 'E', 'E', 'E', 'B', 'E'},
+//        {'3', 'E', 'E', 'E', 'B', 'E', 'E'},
+//        {'E', '4', 'E', 'E', 'E', 'E', 'E'},
 //        {'E', 'E', 'E', 'E', 'E', 'E', 'E'},
-        {'E', 'E', '1', 'E', 'E', 'B', 'E'},
-        {'3', 'E', 'E', 'E', 'E', 'E', 'E'},
-        {'E', 'E', 'E', 'E', 'E', 'E', 'B'},
+//        {'E', 'E', 'E', 'E', 'E', '3', 'E'},
+//        {'E', 'E', 'B', 'E', 'E', 'E', '3'},
+//        {'E', 'B', 'E', 'E', 'E', 'E', 'E'},};
+    public static char[][] board = {
+        {'E', 'E', 'E', 'B', 'E', 'E', 'E'},
         {'E', 'E', 'E', 'E', 'E', 'E', 'E'},
-        {'B', 'E', 'E', 'E', 'E', 'E', 'E'},
-        {'E', 'E', 'E', 'E', 'E', 'E', '3'},
-        {'E', '2', 'E', 'E', 'B', 'E', 'E'},
-    };
+        {'E', 'E', 'E', '4', 'E', 'E', 'E'},
+        {'B', 'E', '4', 'E', '2', 'E', '1'},
+        {'E', 'E', 'E', '3', 'E', 'E', 'E'},
+        {'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+        {'E', 'E', 'E', '1', 'E', 'E', 'E'},};
+//    public static char[][] board = {
+//        {'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//        {'E', 'E', 'B', 'E', '3', 'E', 'E'},
+//        {'E', '2', 'E', 'E', 'E', '2', 'E'},
+//        {'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//        {'E', 'B', 'E', 'E', 'E', '1', 'E'},
+//        {'E', 'E', '4', 'E', '2', 'E', 'E'},
+//        {'E', 'E', 'E', 'E', 'E', 'E', 'E'},};
+
+    public static ArrayList<int[]> blackCell = new ArrayList<int[]>();
+    public static ArrayList<int[]> blackCellSolved = new ArrayList<int[]>();
+    public static ArrayList<int[]> unsolvedCell = new ArrayList<int[]>();
+    public static ArrayList<String> steps = new ArrayList<String>();
 
     /**
      * @param args the command line arguments
      */
-    public static ArrayList<int[]> getTetangga(int[] cell) {
-        ArrayList<int[]> tetangga = new ArrayList<int[]>();
-        // tetangga atas
-        if ((cell[0] - 1) >= 0 && board[(cell[0] - 1)][cell[1]] == 'E') {
-            tetangga.add(new int[]{(cell[0] - 1), cell[1], 0});
+    public static boolean isBlack(int[] cell) {
+        if (board[cell[0]][cell[1]] == 'B' || board[cell[0]][cell[1]] == '1' || board[cell[0]][cell[1]] == '2' || board[cell[0]][cell[1]] == '3' || board[cell[0]][cell[1]] == '4') {
+            return true;
         }
-        // tetangga kanan
-        if ((cell[1] + 1) <= 6 && board[cell[0]][(cell[1] + 1)] == 'E') {
-            tetangga.add(new int[]{(cell[0]), (cell[1] + 1), 1});
-        }
-        // tetangga bawah
-        if ((cell[0] + 1) <= 6 && board[(cell[0] + 1)][cell[1]] == 'E') {
-            tetangga.add(new int[]{(cell[0] + 1), (cell[1]), 2});
-        }
-        // tetangga kiri
-        if ((cell[1] - 1) >= 0 && board[cell[0]][(cell[1] - 1)] == 'E') {
-            tetangga.add(new int[]{(cell[0]), (cell[1] - 1), 3});
-        }
-
-        return tetangga;
+        return false;
     }
 
-    public static void main(String[] args) {
-        // TODO code application logic here
-        ArrayList<int[]> blackCell = new ArrayList<int[]>();
-        ArrayList<int[]> blackCellSolved = new ArrayList<int[]>();
+    public static boolean isEmpty(int[] cell) {
+        if (board[cell[0]][cell[1]] == 'E') {
+            return true;
+        }
+        return false;
+    }
 
+    public static void getBlackCell() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 'B' || board[i][j] == '0' || board[i][j] == '1' || board[i][j] == '2' || board[i][j] == '3' || board[i][j] == '4') {
+                if (isBlack(new int[]{i, j})) {
                     if (board[i][j] == 'B') {
                         blackCell.add(new int[]{i, j, -1});
                     } else {
@@ -73,17 +83,83 @@ public class AkariPuzzle {
                 }
             }
         }
+    }
 
-//        System.out.println("Before : ");
-//        for (int i = 0; i < blackCell.size(); i++) {
-//            System.out.println("Blackcell : " + (i + 1) + " [ " + blackCell.get(i)[0] + ", " + blackCell.get(i)[1] + ", " + blackCell.get(i)[2] + " ]");
-//        }
+    public static void getUnsolvedCell() {
+        unsolvedCell = new ArrayList<int[]>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'E') {
+                    unsolvedCell.add(new int[]{i, j});
+                }
+            }
+        }
+    }
+
+    public static ArrayList<int[]> getNeighbour(int[] cell, String mode) {
+        ArrayList<int[]> neighbour = new ArrayList<int[]>();
+        ArrayList<int[]> lampNeighbour = new ArrayList<int[]>();
+        ArrayList<int[]> allNeighbour = new ArrayList<int[]>();
+        // tetangga atas
+        if ((cell[0] - 1) >= 0) {
+            allNeighbour.add(new int[]{(cell[0] - 1), cell[1], 0});
+            if ((board[(cell[0] - 1)][cell[1]] == 'E' || board[(cell[0] - 1)][cell[1]] == 'O')) {
+                neighbour.add(new int[]{(cell[0] - 1), cell[1], 0});
+            }
+            if ((board[(cell[0] - 1)][cell[1]] == 'O')) {
+                lampNeighbour.add(new int[]{(cell[0] - 1), cell[1], 0});
+            }
+        }
+        // tetangga kanan
+        if ((cell[1] + 1) <= 6) {
+            allNeighbour.add(new int[]{(cell[0]), (cell[1] + 1), 1});
+            if ((board[cell[0]][(cell[1] + 1)] == 'E' || board[cell[0]][(cell[1] + 1)] == 'O')) {
+                neighbour.add(new int[]{(cell[0]), (cell[1] + 1), 1});
+            }
+            if ((board[cell[0]][(cell[1] + 1)] == 'O')) {
+                lampNeighbour.add(new int[]{(cell[0]), (cell[1] + 1), 1});
+            }
+        }
+        // tetangga bawah
+        if ((cell[0] + 1) <= 6) {
+            allNeighbour.add(new int[]{(cell[0] + 1), (cell[1]), 2});
+            if ((board[(cell[0] + 1)][cell[1]] == 'E' || board[(cell[0] + 1)][cell[1]] == 'O')) {
+                neighbour.add(new int[]{(cell[0] + 1), (cell[1]), 2});
+            }
+            if ((board[(cell[0] + 1)][cell[1]] == 'O')) {
+                lampNeighbour.add(new int[]{(cell[0] + 1), (cell[1]), 2});
+            }
+        }
+        // tetangga kiri
+        if ((cell[1] - 1) >= 0) {
+            allNeighbour.add(new int[]{(cell[0]), (cell[1] - 1), 3});
+            if ((board[cell[0]][(cell[1] - 1)] == 'E' || board[cell[0]][(cell[1] - 1)] == 'O')) {
+                neighbour.add(new int[]{(cell[0]), (cell[1] - 1), 3});
+            }
+            if ((board[cell[0]][(cell[1] - 1)] == 'O')) {
+                lampNeighbour.add(new int[]{(cell[0]), (cell[1] - 1), 3});
+            }
+        }
+
+        if (mode.equals("all")) {
+            return allNeighbour;
+        }
+        if (mode.equals("lamp")) {
+            return lampNeighbour;
+        }
+        return neighbour;
+    }
+
+    public static void main(String[] args) {
+
+        // scanning blackcell
+        getBlackCell();
+        // sorting
         int max = 4;
         ArrayList<int[]> temp = new ArrayList<int[]>();
         while (max > -2) {
             for (int i = 0; i < blackCell.size(); i++) {
                 if (blackCell.get(i)[2] == max) {
-//                    blackCell.remove(i);
                     temp.add(new int[]{blackCell.get(i)[0], blackCell.get(i)[1], blackCell.get(i)[2]});
                 }
             }
@@ -91,16 +167,8 @@ public class AkariPuzzle {
         }
         blackCell = temp;
 
-        System.out.println("\nAfter : ");
-        for (int i = 0; i < blackCell.size(); i++) {
-            System.out.println("Blackcell : " + (i + 1) + " [ " + blackCell.get(i)[0] + ", " + blackCell.get(i)[1] + ", " + blackCell.get(i)[2] + " ]");
-            ArrayList<int[]> ttg = getTetangga(blackCell.get(i));
-            for (int j = 0; j < ttg.size(); j++) {
-                System.out.println("Tetangga : " + (j + 1) + " [ " + ttg.get(j)[0] + ", " + ttg.get(j)[1] + ", " + ttg.get(j)[2] + " ]");
-            }
-            System.out.println("");
+        for (int[] b : blackCell){
+            System.out.println("Black Cell : [ "+b[0]+", "+b[1]+", "+b[2]+" ]");
         }
-
     }
-
 }
